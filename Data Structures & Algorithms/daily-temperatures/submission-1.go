020b7b95fce78cc328555624602fd77aa607f72a
Callum Dempsey Leach <callum.leach@hotmail.co.uk>
2026-04-01 20:28:08 +0100

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.go
@@ -0,0 +1,43 @@
+type TemperatureKey struct {
+	Index int
+	Value int
+}
+
+func NewTemperatureKey(index int, value int) TemperatureKey {
+	return TemperatureKey{Index: index, Value: value}
+}
+
+func dailyTemperatures(temperatures []int) []int {
+	// Define the min stack
+	minTemperatureStack := make([]TemperatureKey, 0, len(temperatures))
+	// Define the answer
+	ans := make([]int, len(temperatures))
+
+	for i, temperature := range temperatures {
+		currentTemp := NewTemperatureKey(i, temperature)
+		// Case when the temperature stack has just been initialised
+		if len(minTemperatureStack) == 0 {
+			minTemperatureStack = append(minTemperatureStack, currentTemp)
+			continue
+		}
+		// Case where it is a hotter day, the days that came before it are cooler.
+		// Open a window where we drain the stack.
+		for len(minTemperatureStack) > 0 {
+			// Grep the top of the stack on every iteration of the loop
+			peek := minTemperatureStack[len(minTemperatureStack)-1]
+
+			// Edge case when as we're making our day down the stack, the days get warmer
+			// Where in the stack of 50 40 30, temperature is 32
+			if peek.Value >= currentTemp.Value {
+				break 
+			}
+			ans[peek.Index] = currentTemp.Index - peek.Index
+			minTemperatureStack = minTemperatureStack[:len(minTemperatureStack)-1]
+		}
+
+		// Add the cooler temperature day
+		minTemperatureStack = append(minTemperatureStack, currentTemp)
+	}
+
+	return ans
+}
